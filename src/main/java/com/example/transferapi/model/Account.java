8cd6b226266b1dd6c43689f19bb4699cf4e4cdba
Subo <subodhinisanka@gmail.com>
2024-08-18 01:30:53 +0530

    subodhi06

--- src/main/java/com/example/transferapi/model/Account.java
+++ src/main/java/com/example/transferapi/model/Account.java
@@ -1,27 +1,33 @@
 package com.example.transferapi.model;
 
+//Represents a bank account with an account number and balance.
 public class Account {
     private String accountNumber;
     private double balance;
 
-    // Constructors, getters, and setters
+    // Constructor to initialize the account with account number and balance.
     public Account(String accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
+    //Returns the account number.
     public String getAccountNumber() {
         return accountNumber;
     }
 
+    // Sets the account number.
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
+    //Returns the balance.
     public double getBalance() {
         return balance;
     }
 
+
+    // Sets the balance.
     public void setBalance(double balance) {
         this.balance = balance;
     }
